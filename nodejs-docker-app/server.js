fab74b1041ba2d40a8a4e37fbb7ee422961a0140
leejh96 <dlwngur96@naver.com>
2022-07-24 12:45:57 +0900

    docker 강의

--- nodejs-docker-app/server.js
+++ nodejs-docker-app/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = 8080;
 
-//APP 
+//APP
 const app = express();
-app.get('/', (req,res) => {
-    res.send("반가워요!!!!!!!")
+app.get("/", (req, res) => {
+  res.send("안녕하세요 반가워요 ㅎㅎㅎ!!");
 });
 
 app.listen(PORT);
-console.log("Server is running")
+console.log("Server is running");
